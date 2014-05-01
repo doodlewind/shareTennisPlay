7ebdd2f6f52f6d3c362aef8ffed99f942745a935
ewind <ewind@mail.ustc.edu.cn>
2014-05-02 01:00:42 +0800

    add feature
    Now new score of free game depends on players’ current score.

--- upload_fns.php
+++ upload_fns.php
@@ -1,10 +1,66 @@
 <?php
-function count_value($set_p1,$set_p2){
+function count_double_value($set_p1,$set_p2){
 	
 	if($set_p1>$set_p2){
 		return 27-($set_p1+$set_p2);
 	}else
 		return $set_p1+$set_p2;
+}
+function count_single_value($id_p1,$id_p2,$set_p1,$set_p2,$conn){
+	$p = getRatio($conn,$id_p1,$id_p2);
+	if($set_p1>$set_p2){
+		return $p*(27-($set_p1+$set_p2));
+	}else
+		return $p*($set_p1+$set_p2);
+}
+//积分为7天内->604800
+function getRatio($conn,$id_p1,$id_p2){
+	$sql = '
+		select * from
+		(select id_p1 as id,name,sum(sum_single) as score from(
+			select time,sum(sum_half) as sum_single,name,id_p1 from(
+				select time,sum(value_p1) as sum_half,name_p1 as name,id_p1 from game_free
+				group by name_p1
+				union all
+				select time,sum(value_p2),name_p2,id_p2 from game_free
+				group by name_p2)
+			as sum_hf1
+			group by name
+			union
+			select time,sum(sum_tmp) as sum_double,name,id_p1 from(
+				select time,sum(value_p1n2) as sum_tmp,name_p1 as name,id_p1 from game_double
+				where time
+				group by name_p1 union all
+				select time,sum(value_p1n2),name_p2 as name,id_p2 from game_double
+				group by name_p2 union all
+				select time,sum(value_p3n4),name_p3 as name,id_p3 from game_double
+				group by name_p3 union all
+				select time,sum(value_p3n4),name_p4 as name,id_p4 from game_double
+				group by name_p4)
+			as sum_hf2
+			group by name)as sum_hf where UNIX_TIMESTAMP()-time < 604800
+		group by name)as rank
+		where id="';
+		
+	$res = $conn->query($sql.$id_p1.'";');
+	$score1 = $res->fetch_assoc()['score'];
+
+	$res = $conn->query($sql.$id_p2.'";');
+	$score2 = $res->fetch_assoc()['score'];
+	
+	//即便sql查找没有找到id_p1，判断仍可用
+	if($score1==0){
+		$score1 = 6;
+	}
+	
+	$p = (float)$score2/(float)$score1;
+	if($p < 0.9){
+		$p = 0.9;
+	}
+	else if($p > 3){
+		$p = 3;
+	}
+	return $p;
 }
 function update_password($conn){
 	$password = $_POST['password'];
@@ -37,10 +93,11 @@ function upload_free($flag,$conn){
 		$set_p1 = $_POST['set_p1'];
 		$set_p2 = $_POST['set_p2'];
 		$time = strtotime( date("Y",time())."-".$month."-".$day." ".$hour.":00:".date("s"));
-		$value_p1 = count_value($set_p1,$set_p2);
-		$value_p2 = count_value($set_p2,$set_p1);
 		$result = $conn->query('select id_ustc from user where name="'.$name_p2.'";');
 		$id_p2 = $result->fetch_assoc()['id_ustc'];
+		$value_p1 = count_single_value($id_p1,$id_p2,$set_p1,$set_p2,$conn);
+		$value_p2 = count_single_value($id_p2,$id_p1,$set_p2,$set_p1,$conn);
+		
 		$result = $conn->query('select name from user where id_ustc="'.$id_p1.'";');
 		$name_p1 = $result->fetch_assoc()['name'];
 		
@@ -61,8 +118,8 @@ function upload_free($flag,$conn){
 		$set_p1n2 = $_POST['set_p1n2'];
 		$set_p3n4 = $_POST['set_p3n4'];
 		$time = strtotime( date("Y",time())."-".$month."-".$day." ".$hour.":00:00");
-		$value_p1n2 = count_value($set_p1n2,$set_p3n4);	
-		$value_p3n4 = count_value($set_p3n4,$set_p1n2);
+		$value_p1n2 = count_double_value($set_p1n2,$set_p3n4);
+		$value_p3n4 = count_double_value($set_p3n4,$set_p1n2);
 		$result = $conn->query('select name from user where id_ustc="'.$_SESSION['valid_id_ustc'].'";');
 		$name_p1 = $result->fetch_assoc()['name'];
 		
